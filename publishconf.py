ad69cd5327d103638b877e5b9056f54ccc671e87
Justin Cooksey <justin@cooksey.com.au>
2023-07-30 15:43:29 +1000

    Publish mk2

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://jscooksey.github.io/Pelican/"
+SITEURL = "https://justincooksey.com
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
